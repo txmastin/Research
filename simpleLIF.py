063964ca6bd5987767983c653e16e5c1a1a62d0e
Tucker Mastin <quartalcounterpoint@gmail.com>
2024-09-20 16:31:09 -0700

    experimented with indexing neurons in network

--- simpleLIF.py
+++ simpleLIF.py
@@ -9,11 +9,29 @@ import numpy
 # from IPython.display import HTML
 from plotLIF import *
 
+class leakyNeuron:
+    def __init__(self, beta, threshold):
+        self.lif = snn.Leaky(beta=beta, threshold=threshold)
+        self.mem = torch.zeros(1)
+        self.spk = torch.zeros(1)
+        self.mem_rec = []
+        self.spk_rec = []
+
 
 beta = 0.8
+threshold = 1
 
-lif1 = snn.Leaky(beta=beta, threshold=1) # LIF neuron with a decay rate of beta
-lif2 = snn.Leaky(beta=beta, threshold=1)
+#lif1 = snn.Leaky(beta=beta, threshold=1) # LIF neuron with a decay rate of beta
+#lif2 = snn.Leaky(beta=beta, threshold=1)
+
+width = 1
+depth = 2
+
+
+net = [[leakyNeuron(beta, threshold) for i in range(depth)] for j in range(width)]
+
+lif1 = net[0][0]
+lif2 = net[0][1]
 
 # setup inputs
 num_steps = 200
@@ -24,7 +42,7 @@ num_steps = 200
 
 x = spikegen.rate_conv(torch.from_numpy(numpy.random.rand(num_steps)*0.25))
 
-
+'''
 mem1 = torch.zeros(1)
 spk1 = torch.zeros(1)
 mem1_rec = []
@@ -49,13 +67,31 @@ for step in range(num_steps):
     spk2_rec.append(spk2)
 
 
+for step in range(num_steps):
+    lif1.spk, lif1.mem = lif1.lif(x[step], lif1.mem)
+    lif2.spk, lif2.mem = lif2.lif(lif1.spk, lif2.mem)
+    
+    lif1.mem_rec.append(lif1.mem)
+    lif1.spk_rec.append(lif1.spk)
+    lif2.mem_rec.append(lif2.mem)
+    lif2.spk_rec.append(lif2.spk)
+'''
+for step in range(num_steps):
+    for layer in net:
+        layer[0].spk, layer[0].mem = layer[0].lif(x[step], layer[0].mem)
+        layer[1].spk, layer[1].mem = layer[1].lif(layer[0].spk, layer[1].mem)
+        for neuron in layer:
+            neuron.mem_rec.append(neuron.mem)
+            neuron.spk_rec.append(neuron.spk)
+            
+mem1_rec = torch.stack(net[0][0].mem_rec)
+spk1_rec = torch.stack(net[0][0].spk_rec)
 
-mem1_rec = torch.stack(mem1_rec)
-spk1_rec = torch.stack(spk1_rec)
+mem2_rec = torch.stack(net[0][1].mem_rec)
+spk2_rec = torch.stack(net[0][1].spk_rec)
 
-mem2_rec = torch.stack(mem2_rec)
-spk2_rec = torch.stack(spk2_rec)
-
+print(spk1_rec)
+print(spk2_rec)
 
 plot2_cur_mem_spk(x, mem1_rec, mem2_rec, spk1_rec, spk2_rec, thr_line=1, ylim_max1=1.0, title="snn.Leaky Neuron Model")
 
