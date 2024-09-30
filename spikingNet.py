30c3f2c57f0a79426da0144dc10cdbd377b9f963
Tucker Mastin <quartalcounterpoint@gmail.com>
2024-09-30 13:04:30 -0700

    added stdp update rules

--- spikingNet.py
+++ spikingNet.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 beta = 0.95
 threshold = 1
 
-layers = 10 
-width = 10
+layers = 3 
+width = 10 
 
 num_trials = 1 
 time_steps = 100000
@@ -32,8 +32,21 @@ class SpikingNetwork(nn.Module):
         self.fc_layers = nn.ModuleList([
             nn.Linear(num_neurons, num_neurons) for _ in range(num_layers)
         ])
+    
+    def stdp_update(self, pre_spikes, post_spikes, weights, lr):
+        delta_t = post_spikes - pre_spikes
         
-    def forward(self, x):
+        # long term potentiation (aka reinforcement)
+        ltp_mask = (delta_t > 0).float()
+        weights += lr*ltp_mask
+        
+        # long term depression
+        ltd_mask = (delta_t < 0).float()
+        weights -= lr*ltd_mask
+
+        return weights
+
+    def forward(self, x, lr):
         mem = []
         spikes = []
         
@@ -51,6 +64,8 @@ class SpikingNetwork(nn.Module):
             # Fully connected layer (except for the last layer)
             if i < self.num_layers - 1:
                 x_in = self.fc_layers[i](spike)
+
+                self.fc_layers[i].weight.data = self.stdp_update(x_in, spike, self.fc_layers[i].weight.data, lr)
             
         # Feedback connection from output layer to input layer (last layer)
         feedback_input = spikes[-1]
@@ -68,14 +83,14 @@ in_avalanche = False
 current_avalanche = 0
 
 feedback_input = torch.zeros(width)
-
-for trial in range(num_trials):
+lr = [0.0, 0.2, 0.8]
+for rate in lr:
     for t in range(time_steps):
 
         spontaneous_spikes = (torch.rand(width) < spontaneous_prob).float()
         # rate_coded_spikes = spikegen.rate_conv(torch.rand(width))  
 
-        spikes, mem, feedback_input = net(spontaneous_spikes+feedback_input)
+        spikes, mem, feedback_input = net(spontaneous_spikes+feedback_input, rate)
         
         if torch.cat(spikes).sum().item() > 0:
             in_avalanche = True
@@ -89,7 +104,13 @@ for trial in range(num_trials):
     avalanche, count = np.unique(avalanche_count, return_counts=True)
 
         # Plotting
-    plt.scatter(avalanche, count, color='blue', alpha=0.7)
+    if rate == 0.0:
+        plt.scatter(avalanche, count, color='blue', alpha=0.7)
+    if rate == 0.2:
+        plt.scatter(avalanche, count, color='red', alpha=0.7)
+    if rate == 0.8:
+        plt.scatter(avalanche, count, color='black', alpha=0.7)
+
 
 plt.title('Neuronal Avalanche Size Distribution')
 plt.xlabel('Avalanche Size')
